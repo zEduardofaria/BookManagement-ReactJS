2b97b45fe8e654769c7a0b676aa228edbd7a8dc4
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-19 10:26:53 -0300

    Component input added

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import Input from './componentes/Input';
 
 class App extends Component {
 
@@ -90,25 +91,17 @@ class App extends Component {
           <div className="content" id="content">
                 <div className="pure-form pure-form-aligned">
                   <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                    <div className="pure-control-group">
-                      <label htmlFor="nome">Nome</label> 
-                      <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />                  
-                    </div>
-                    <div className="pure-control-group">
-                      <label htmlFor="email">Email</label> 
-                      <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />                  
-                    </div>
-                    <div className="pure-control-group">
-                      <label htmlFor="senha">Senha</label> 
-                      <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />                                      
-                    </div>
+                    <Input id="nome" type="text" label="Nome" name="nome" value={this.state.nome} onChange={this.setNome} />
+                    <Input id="email" type="email" label="Email" name="email" value={this.state.email} onChange={this.setEmail} />
+                    <Input id="senha" type="password" label="Senha" name="senha" value={this.state.senha} onChange={this.setSenha} />
+
                     <div className="pure-control-group">                                  
                       <label></label> 
                       <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
                     </div>
                   </form>             
+                </div>
   
-                </div>  
                 <div>            
                   <table className="pure-table">
                     <thead>
---
+++ cdc-admin/src/componentes/Input.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+
+    render() {
+        const { id, type, name, value, value, onChange, label } = this.props
+
+        return (
+            <div className="pure-control-group">
+              <label htmlFor={id}>{label}</label> 
+              <input id={id} type={type} name={name} value={value} onChange={onChange} />                  
+            </div>
+        )
+    }
+}
+
+export default Input;
